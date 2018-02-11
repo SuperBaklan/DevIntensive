42c67ae4bf7fd8ccc917d21e394e267e36bef3f5
SuperBaklan <ivan.bklanov@gmail.com>
2018-02-12 01:36:56 +0300

    Демонстрация циклов приложения

---
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -0,0 +1,10 @@
+package com.softdesign.devintensive.utils;
+
+/**
+ * Created by Android on 11.02.2018.
+ */
+
+public interface ConstantManager {
+    String TAG_PREFIX = "DEV ";
+    String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+}
